83205bd99b765073a3e7206624c9136a16445ce0
Sultan Mukhtarov <suli419956@gmail.com>
2024-07-27 12:50:59 +0600

    two way binding created.

--- src/Pages/CategoriesForm.tsx
+++ src/Pages/CategoriesForm.tsx
@@ -1,18 +1,37 @@
+import React, {useState} from 'react';
 
 
 const CategoriesForm = () => {
 
+  const [category, setCategory] = useState({
+    name: '',
+    CategoryType: '',
+  });
+
+  const onFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCategory((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  console.log(category)
+
 
   return (
-    <form className="container mb-4 mt-3">
+    <form className="container mb-4 mt-3" onSubmit={onSubmit}>
       <h4>Add Category</h4>
       <div className="form-group">
-        <label htmlFor="name">Food</label>
+        <label htmlFor="name">Name</label>
         <input
           required
           type="text"
-          name="name"
           id="name"
+          name="name"
+          value={category.name}
+          onChange={onFieldChange}
           className="form-control"
         />
       </div>
@@ -22,13 +41,15 @@ const CategoriesForm = () => {
           required
           type="text"
           name="CategoryType"
+          value={category.CategoryType}
+          onChange={onFieldChange}
           id="CategoryType"
           className="form-control"
         />
       </div>
       <div className="mt-3">
-      <button className="btn btn-danger">Edit</button>
-      <button type="submit" className="btn btn-primary ms-3">Create</button>
+      <button className="btn btn-primary">Edit</button>
+      <button type="submit" className="btn btn-success ms-3">Create</button>
       </div>
     </form>
   );
